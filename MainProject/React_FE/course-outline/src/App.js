a40a1f76989cd745b3b393445081b067e2e0a65d
Adam D'Andrea <adamdand@telus.net>
2020-12-15 16:58:43 -0700

    updated table of learning outcomes to better format

--- MainProject/React_FE/course-outline/src/App.js
+++ MainProject/React_FE/course-outline/src/App.js
@@ -4,6 +4,22 @@ import './App.css';
 import * as ReactBootStrap from "react-bootstrap";
 import { useReducer, useState } from 'react';
 
+function myCreateFunction() {
+  var table = document.getElementById("myTable");
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "NEW CELL1";
+  cell2.innerHTML = "NEW CELL2";
+}
+
+
+
+
+function myDeleteFunction() {
+  document.getElementById("myTable").deleteRow(0);
+}
+
 function addOutComeBtn(){
   alert("learning outcome ADDED!")
 }
@@ -40,39 +56,69 @@ const App = () => {
 
   return (
     <div className="App">
-        <ReactBootStrap.Table striped bordered hover>
-  <thead>
-  <b>2. Learning Outcomes</b>
-  <tr>At the end of this course, you will be able to:</tr>
-    <tr>
-      <th>Number</th>
-      <th>Description</th>
-    </tr>
-  </thead>
-  <tbody>
-    {LearningOutComes.map(renderLearningOutComes)}
+      <tr>At the end of this course, you will be able to:</tr>
+    <table className="table" id="myTable">
+        <thead>
+          <th>Number</th>
+          <th>Learning Outcome Description</th>
+        </thead>
 
-  </tbody>
-  </ReactBootStrap.Table>
-  <div>
-    <h1>New Learning Outcome Input:</h1>
-    <input
-                       className="input"
-                       type="text"
-                       placeholder="Input new learning outcomecout"
-                       //value={inputValue}
-                       //onChange={(e) => setInputValue(e.target.value)}
-                       //onKeyDown={handleKeyDown}
-                       />
-      <button onClick={addOutComeBtn}>
-        Add Learning Outcome
-      </button>
-      <button onClick={deleteOutComeBtn}>
-        Delete Learning Outcome
-      </button>
+        <tbody>
+        {LearningOutComes.map(renderLearningOutComes)}
+        </tbody>
+      </table>
+
+
+
+      {/* <div class="field has-addons">
+        <p class="control">
+          <input class="input" type="text" placeholder="Component" />
+          <input class="input" type="text" placeholder="Learning Outcome(s)" />
+          <input class="input" type="text" placeholder="Weight" />
+        </p>
+        <p class="control">
+          <a class="button is-info">
+            Add
+          </a>
+
+          <a class="button is-info">
+            Delete
+          </a>
+        </p>
+      </div> */}
+
+      <div class="field has-addons">
+        <p class="control">
+          <input class="input" type="text" placeholder="Number" />
+        </p>
+        <p class="control">
+          <input class="input" type="text" placeholder="Description" />
+        </p>
+      </div>
+
+      <div class="field has-addons">
+        
+      </div>
+
+      <div class="field is-grouped">
+        <p class="control">
+          <a class="button is-primary"
+          onClick = {myCreateFunction}>
+            Add
+          </a>
+        </p>
+        <p class="control">
+          <a class="button is-light"
+          onClick = {myDeleteFunction}>
+            Delete
+          </a>
+        </p>
+      </div>
+
+
+
+
     </div>
-    </div>
-    
   );
 }
 
