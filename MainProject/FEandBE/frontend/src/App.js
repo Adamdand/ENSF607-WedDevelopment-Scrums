c55b054c0dd4f7582bb21583931832a64478b523
Adam D'Andrea <58803595+Adamdand@users.noreply.github.com>
2021-01-14 20:42:29 -0700

    almost have react to pdf button complete

--- MainProject/FEandBE/frontend/src/App.js
+++ MainProject/FEandBE/frontend/src/App.js
@@ -6,21 +6,30 @@
  import TimeTable from "./components/timetables/TimeTable";
  import Instructors from "./components/instructors/Instructors";
  import GradesTable from "./components/gradesTable/GradeTable";
+ import myPdf from "./components/myPDF/myPdf";
  import Form from './components/calendarInfo/Form';
+
  import './App.css';
+
+
+ 
 
 
  class App extends Component {
    render() {
      return (
+
+
        <div>
+         
          <Form></Form>
          <LearningOutcomes></LearningOutcomes>
          <TimeTable></TimeTable>
          <Instructors></Instructors>
          <GradeOutcomes></GradeOutcomes>
          <GradesTable></GradesTable>
-
+         <myPdf></myPdf>
+         
          
        </div>
      );
